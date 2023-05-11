001f387e73f271ebd0187028d1c9113f755e0342
sof <130997583+sofhkkj@users.noreply.github.com>
2023-05-11 19:51:10 -0300

    Update script.js

--- códigos/script.js
+++ códigos/script.js
@@ -1,1 +1,17 @@
+function fogodeartificio() {
+    var msg = window.document.getElementById('mensagem')
+    msg.innerHTML = 'Fogo de artificio'
+}
+function funcao() {
+    alert ('aperte ok')
+}
 
+sobre.js
+
+function fogodeartificio() {
+    var msg = window.document.getElementById('mensagem')
+    msg.innerHTML = 'Lorem ipsum'
+}
+function funcao() {
+    alert ('Para saber mais clique em ok')
+}
